3e88ffa704e170f282a6673c08bae00fb8862804
Maksim <maksimkuzin73@yandex.ru>
2024-11-28 09:26:21 +0400

    fix pz

--- wwwroot/SQLQuery2.sql
+++ wwwroot/SQLQuery2.sql
@@ -4,17 +4,17 @@ CREATE PROCEDURE AddAlert
     @Date DATETIME,
     @Severity NVARCHAR(30),
     @Status NVARCHAR(30),
-    @HardDriveId INT -- Передаем только один HardDriveId
+    @HardDriveId INT 
 AS
 BEGIN
-    -- Вставка записи в таблицу Alerts
-    INSERT INTO Alerts (Type, Description, Date, Severity, Status)
-    VALUES (@Type, @Description, @Date, @Severity, @Status);
+    IF EXISTS (SELECT 1 FROM HardDrives WHERE Id = @HardDriveId)
+    BEGIN
+        INSERT INTO Alerts (Type, Description, Date, Severity, Status)
+        VALUES (@Type, @Description, @Date, @Severity, @Status);
     
-    -- Получаем ID вставленной записи
-    DECLARE @AlertId INT = SCOPE_IDENTITY();
+        DECLARE @AlertId INT = SCOPE_IDENTITY();
 
-    -- Вставляем запись в таблицу связи AlertsHardDrives
-    INSERT INTO AlertHardDrive (AlertsId, HardDrivesId)
-    VALUES (@AlertId, @HardDriveId);
+        INSERT INTO AlertHardDrive (AlertsId, HardDrivesId)
+        VALUES (@AlertId, @HardDriveId);
+    END
 END
